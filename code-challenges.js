2cce1a131fdfe4552d704e2a5648ee40bef52268
mabrouk10 <samimabrouk10@gmail.com>
2020-12-12 10:21:12 -0800

    week6

--- code-challenges.js
+++ code-challenges.js
@@ -10,7 +10,42 @@ var people = [
 ]
 // Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
 
+const sent = (obj) => {
 
+  let length = obj.length
+
+  let arrNames = obj.map(value => value.name)
+  // let arrCap = []
+
+  // for (let i = 0; i < arrNames.length; i++) {
+  //   arrCap.push(arrNames[i].split(" "))
+  // }
+
+  let arrCap = arrNames.map(value => value.split(" "))
+
+  let name1 = arrCap.map(value => value[0].charAt(0).toUpperCase() + value[0].slice(1))
+  let name2 = arrCap.map(value => value[1].charAt(0).toUpperCase() + value[1].slice(1))
+
+  let names = name1.concat(name2)
+
+  let occ = obj.map(value => value.occupation)
+
+  let finalArray = []
+
+  for (let i = 0; i < length; i++) {
+    finalArray.push(`"${name1[i]} ${name2[i]} is a ${occ[i]}." `)
+  }
+
+  return finalArray.join("")
+
+  // console.log(arrNames)
+  // console.log(arrCap)
+  // console.log(name1)
+  // console.log(name2)
+  // console.log(names)
+}
+
+console.log(sent(people))
 
 
 
@@ -22,7 +57,24 @@ var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
 
+const rem = (arr) => {
+  
+  let newArray = []
 
+  for (let i = 0; i < arr.length; i++) {
+    let remainder = 0
+    if(typeof arr[i] === 'number'){
+    remainder = arr[i] % 3
+    // console.log(remainder)
+    newArray.push(remainder)
+    // return arr[i]
+    }
+  }
+  return newArray
+}
+
+console.log(rem(testingArray1))
+console.log(rem(testingArray2))
 
 
 // --------------------3) Create a function that takes in two arrays as arguments returns one array with no duplicate values.
@@ -30,3 +82,12 @@ var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
 var testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
 var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
 // Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
+
+
+const dup = (arr1, arr2) => {
+
+  let arr3 = arr2.concat(arr1)
+  let arr4 = arr3.filter((value, index) => arr3.indexOf(value) === index)
+  return arr4
+}
+console.log(dup(testingArray4, testingArray3))
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,30 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) Below is the controller for the app. This is the C in the MVC model. This is where the program will communicate with both the database and the view. The controller will interact directly with the database and grab whatever data needed and pass it along to where it is needed in the view. 
+
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2) The index is used to view all the entries that are in the database hence the '.all' found at the end of BlogPost
+
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3) The show method will allow the user to look into one specific entry in the database. The find method with the id being passed down into the params. Allows you to go and look for a specific entry in the database with the same id.
+
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) The new method will take the user to a form page. In the form page a user can start the creation of a new entry into the database. However the new method does not allow you to actually enter the database. It is more so staging the entry you want to make
+
   def new
     @post = Post.new
   end
 
   def create
-    # 5)
+    # 5) As mentioned above the new method does not allow the user to actually make an entry to the database. However the create method below will actually add the 'staged entry' from the new method. It does this by using the .create method. If the post is valid the user will be redirected to the new post page. If it isnt valid the user will be directed back to the new method page where the user will find the form once again
+
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -39,7 +44,8 @@ class BlogPostsController < ApplicationController
 
   def update
     @post = BlogPost.find(params[:id])
-    # 6)
+    # 6) The update method is used to update or put/patch an entry in the database. By looking thru the logic of the function. You can see that it will first take the id of the entry you want to update. If the update is valid. It will then update the entry in the database.
+
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -53,15 +59,18 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # 7)
+      # 7) If the destroy logic above was not valid. The user will be taken back to the post they tried to delete.
+
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 8)
+  # 8) The private method below is known as strong params. The strong param method will ensure that the database will not be hacked or attacked. It will only allow certains types of entries to be made. In order for the database to stay protected. 
+
   private
   def blog_post_params
-    # 9)
+    # 9) The logic below ensures that the only being allowed into the blog_post database will be a :title column and a :content column
+
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -71,6 +80,7 @@ end
 # FILE: app/models/blog_post.rb
 
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10) The blogpost table is linked to another model called comments. Which means the blogpost is allowed to "has_many" comments to it along with the title and content data it already has. The comments will "belong_to" a single BlogPost. And it does this by referring to the Blogpost id. Which is how the two models will be connected. 
+
   has_many :comments
 end
