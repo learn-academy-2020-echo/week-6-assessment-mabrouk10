07adf8267d3a59045ebc0995118d1d77ff188bb9
mabrouk10 <samimabrouk10@gmail.com>
2020-12-31 22:13:10 -0800

    revised assessment

--- code-challenges.js
+++ code-challenges.js
@@ -12,30 +12,36 @@ var people = [
 
 const sent = (obj) => {
 
+  //capture the length of the object being passed down in order to use in future loops
   let length = obj.length
 
+  //capture all the "names" in the object
   let arrNames = obj.map(value => value.name)
-  // let arrCap = []
 
-  // for (let i = 0; i < arrNames.length; i++) {
-  //   arrCap.push(arrNames[i].split(" "))
-  // }
-
+  //seperate the first and last names 
   let arrCap = arrNames.map(value => value.split(" "))
 
+  //once the first and last names are seperated we can now go in and captalize the first letter.
   let name1 = arrCap.map(value => value[0].charAt(0).toUpperCase() + value[0].slice(1))
   let name2 = arrCap.map(value => value[1].charAt(0).toUpperCase() + value[1].slice(1))
 
+  //once they are captilized we can now join them back together using cocat. now we have a names array that has the first and last names capatilzied
   let names = name1.concat(name2)
 
+  //capture the occupations of the obj and put it into an array
   let occ = obj.map(value => value.occupation)
 
+  //have an empty array ready to push in the first name last name and occupation all together into a sentence. By doing this we can create a sentence with every entry in the object
   let finalArray = []
 
   for (let i = 0; i < length; i++) {
-    finalArray.push(`"${name1[i]} ${name2[i]} is a ${occ[i]}." `)
+
+    // finalArray.push(`"${name1[i]} ${name2[i]} is a ${occ[i]}." `)
+    //inside of the for loop we now can compose a sentence using the names and occ array we created earlier that helped isolate the names to captialize and complete the sentence with the occ array we obtained
+    finalArray.push(`"${names[i]} is a ${occ[i]}." `)
   }
 
+  //join it all together using join to make a complete sentence
   return finalArray.join("")
 
   // console.log(arrNames)
@@ -59,17 +65,23 @@ var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
 
 const rem = (arr) => {
   
+  //have empty array ready to push into
   let newArray = []
 
   for (let i = 0; i < arr.length; i++) {
+    //declaring a variable that can be changed everytime it goes thru loops. this will allow us to store the remainder found with every iteration
     let remainder = 0
+    //check to see if the iteration is a number or not. since words and booleans cannot have remainders
     if(typeof arr[i] === 'number'){
+    //if the iteration is number we can now use the modulus to find the remainder
     remainder = arr[i] % 3
     // console.log(remainder)
+    //once the remainder is found we can now store it into our array
     newArray.push(remainder)
     // return arr[i]
     }
   }
+  //return our new array
   return newArray
 }
 
@@ -85,6 +97,14 @@ var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
 
 
 const dup = (arr1, arr2) => {
+
+//First time around in the filter method
+//3 will be in position 0
+//indexOf will always get the position of the first instance of the array
+// then it will compare it to the re-iteration of it
+// so the second instant of 3 happens at position 6
+// however indexOf 3 is still 0 since indexOf gets the first position of the 3 occurence
+// that is why indexOf 3 will not equal to the second instance of 3 which is at 6
 
   let arr3 = arr2.concat(arr1)
   let arr4 = arr3.filter((value, index) => arr3.indexOf(value) === index)
